8d5192673541bb2c3faab788dd5ba2209c0d1ae3
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2023-09-13 17:19:02 +0200

    save

---
+++ script.js
@@ -0,0 +1,5 @@
+const userName = prompt("What is your name ?");
+const age = prompt("What is your age ?");
+const city = prompt("Where do you live ?");
+
+alert(`Hello, ${userName}! You are ${age} years old and you live in ${city}`);
