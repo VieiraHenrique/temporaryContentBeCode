bc2c7525271ca32d18ffac962d4ae3cd819597eb
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2023-10-09 09:01:11 +0200

    save

--- WEEK6/playground/smallTests/script.js
+++ WEEK6/playground/smallTests/script.js
@@ -1,80 +1,10 @@
-/* const fakeCallToAnAPI = (url, success, failure) => {
-  const delay = Math.floor(Math.random() * 4500) + 500;
-  setTimeout(() => {
-    if (delay > 4000) {
-      failure("Connection Timeout ! :(");
-    } else {
-      success(`Here is your fake data from ${url}`);
-    }
-  }, delay);
+const myObj = {
+  firstName: "Henrique",
+  lastName: "Vieira",
+  age: 35,
+  hasPets: false,
 };
 
-fakeCallToAnAPI(
-  "api/movies",
-  // The callback if it's a success
-  (msg) => {
-    console.log("SUCCESS (1)");
-    console.log(msg);
-    fakeCallToAnAPI(
-      "api/movies/7",
-      // The callback if it's a success
-      (msg) => {
-        console.log("SUCCESS (2)");
-        console.log(msg);
-        fakeCallToAnAPI(
-          "api/pictures/7",
-          // The callback if it's a success
-          (msg) => {
-            console.log("SUCCESS (3)");
-            console.log(msg);
-          }, // The callback if there was an error
-          (msg) => {
-            console.log("ERROR (3)");
-            console.log(msg);
-          }
-        );
-      }, // The callback if there was an error
-      (msg) => {
-        console.log("ERROR (2)");
-        console.log(msg);
-      }
-    );
-  }, // The callback if there was an error
-  (msg) => {
-    console.log("ERROR (1)");
-    console.log(msg);
-  }
-);
- */
+const myJson = JSON.stringify(myObj);
 
-const fakeCallToAnAPIPromises = (url) => {
-  return new Promise((resolve, reject) => {
-    const delay = Math.floor(Math.random() * 4500) + 500;
-    setTimeout(() => {
-      if (delay > 4000) {
-        reject("Connection Timeout ! :(");
-      } else {
-        resolve(`Here is your fake data from ${url}`);
-      }
-    }, delay);
-  });
-};
-
-fakeCallToAnAPIPromises("api/movies")
-  .then((msg) => {
-    console.log("SUCCESS (1)");
-    console.log(msg);
-    return fakeCallToAnAPIPromises("api/movies/7");
-  })
-  .then((msg) => {
-    console.log("SUCCESS (2)");
-    console.log(msg);
-    return fakeCallToAnAPIPromises("api/pictures/7");
-  })
-  .then((msg) => {
-    console.log("SUCCESS (3)");
-    console.log(msg);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+console.log(myJson);
