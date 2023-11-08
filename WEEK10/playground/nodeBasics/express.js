b88d9d57b2c12a2176c18325bcae4d6a10b45cd3
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2023-11-08 15:18:56 +0100

    save

--- WEEK10/playground/nodeBasics/express.js
+++ WEEK10/playground/nodeBasics/express.js
@@ -1,17 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const app = require("express")();
 
-// The "express" function returns an object that contains many methods such as 'listen', 'get', 'post', 'use', etc.
-const app = express();
+const verifyPassword = (req, res, next) => {
+  const { password } = req.query;
+  if (password === "IAmTheBigBoss") {
+    next();
+  } else {
+    res.send("Not authorized");
+  }
+};
 
-app.use(bodyParser.urlencoded());
-
-// 'listen' establishes a port to be listened and a callback function that fires as soon as the server is launched.
-app.listen(3000, () => {
-  console.log("Listening to port 3000");
-});
-
-// Just so we don't have to write down {root: __dirname} at every request
 const root = __dirname;
 
 app.get("/", (req, res) => {
@@ -22,22 +19,14 @@ app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/secret", verifyPassword, (req, res) => {
+  res.sendFile("./views/secret.html", { root });
 });
 
-const obj = {
-  school: "BeCode",
-  year: 2023,
-};
-
-app.post("/", (req, res) => {
-  // Sends json back
-  console.log(req.body);
-  res.json(obj);
-});
-
-// Remember that the code will run untill it finds a match. "use" is a match for any request. So in this case, we want to put it at the end as a "catch all"
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root });
+  res.sendFile("./views/404.html", { root });
+});
+
+app.listen(3000, () => {
+  console.log("listening to port 3000");
 });
