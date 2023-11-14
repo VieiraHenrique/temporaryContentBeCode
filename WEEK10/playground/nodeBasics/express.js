c9b12a5ed71ddc75c7f53365a83d25c79c78d8bb
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2023-11-14 11:41:01 +0100

    save

--- WEEK10/playground/nodeBasics/express.js
+++ WEEK10/playground/nodeBasics/express.js
@@ -1,32 +1,62 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const uuid = require("uuid");
 
-const verifyPassword = (req, res, next) => {
-  const { password } = req.query;
-  if (password === "IAmTheBigBoss") {
-    next();
+// Initial "database"
+const users = [];
+
+// Parse responses
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Get all users
+
+app.get("/api/users", (req, res) => res.json(users));
+
+// Get single user
+
+app.get("/api/users/:id", (req, res) => {
+  const memberExists = users.some((user) => {
+    return user.id === req.params.id;
+  });
+
+  if (memberExists) {
+    const singleUser = users.filter((user) => {
+      return user.id === req.params.id;
+    });
+    res.json(singleUser);
   } else {
-    res.send("Not authorized");
+    res.status(400).json({ message: "User not found" });
   }
-};
-
-const root = __dirname;
-
-app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root });
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root });
+// Create new user
+
+app.post("/api/users", (req, res) => {
+  const { username, firstName, lastName, age, isAdmin } = req.body;
+
+  if (!username || !firstName || !lastName || !age || !isAdmin) {
+    res.send(400, { message: "All field are required" });
+  } else {
+    const newUser = {
+      id: uuid.v4(),
+      username,
+      firstName,
+      lastName,
+      age,
+      isAdmin,
+    };
+
+    users.push(newUser);
+    res.json(newUser);
+  }
 });
 
-app.get("/secret", verifyPassword, (req, res) => {
-  res.sendFile("./views/secret.html", { root });
-});
+// Start server
 
-app.use((req, res) => {
-  res.sendFile("./views/404.html", { root });
-});
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("listening to port 3000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
